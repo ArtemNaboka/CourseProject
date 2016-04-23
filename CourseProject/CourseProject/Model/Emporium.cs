8d295c938e7e4b4a9cce69c43ed6146246101755
ArtemNaboka <sir.naboka2013@yandex.ua>
2016-04-23 20:10:57 +0300

    Added files via upload

---
+++ CourseProject/CourseProject/Model/Emporium.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject.Model
+{
+    class Emporium
+    {
+        string name;
+        string address;
+        string phone;
+        struct TimeOfStart
+        {
+            int hours;
+            int minutes;
+        }
+        struct TimeOfFinish
+        {
+            int hours;
+            int minutes;
+        }
+    }
+}
---
+++ CourseProject/CourseProject/Model/EmporiumCollection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject.Model
+{
+    class EmporiumCollection
+    {
+        List<Emporium> emporiums = new List<Emporium>();
+        public Emporium this[int i]
+        {
+            get
+            {
+                return emporiums[i];
+            }
+        }
+       
+
+        //LIST
+        
+        public void Addd(Emporium emp)
+        {
+            emporiums.Add(emp);
+        }
+        public int Findd(Emporium emp)
+        {
+            for(int i = 0; i < emporiums.Count; i++)
+            {
+                if(emporiums[i].Equals(emporiums))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public void Delete(Emporium emp)
+        {
+            emporiums.Remove(emp);
+        }
+    }
+}
